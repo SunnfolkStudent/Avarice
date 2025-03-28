126782e78d8dccbbbe61791101fdf0f03840ebb5
Markus Lange <markus@gamedevteacher.com>
2025-03-28 14:24:30 +0100

    Added Main menu & Made game playable

---
+++ Assets/Scripts/New_Systems/SceneController.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Scripts.New_Systems
+{
+    public class SceneController : MonoBehaviour
+    {
+        public void LoadScene(string sceneName)
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+
+        public void LoadNextScene()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+
+        public void QuitGame()
+        {
+            Application.Quit();
+        }
+    }
+}
